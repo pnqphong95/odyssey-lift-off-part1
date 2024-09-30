956add78e763ac8a1446187aeeaceb253cff2d40
Phong Pham <phamphongbsn@gmail.com>
2024-09-30 12:49:10 +0700

    feat: apollo server + mock data

---
+++ server/src/schema.ts
@@ -0,0 +1,27 @@
+import gpl from "graphql-tag";
+
+export const typeDefs = gpl`
+
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+
+`;
