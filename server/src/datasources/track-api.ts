3f1f943d47a6d747bb69b29c578ff2966cb8f934
Phong Pham <phamphongbsn@gmail.com>
2024-09-30 15:53:19 +0700

    feat: connect live datasources + ts codegen

---
+++ server/src/datasources/track-api.ts
@@ -0,0 +1,15 @@
+import { RESTDataSource } from "@apollo/datasource-rest";
+import { AuthorModel, TrackModel } from "../models";
+
+export class TrackAPI extends RESTDataSource {
+  baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+
+  getTracksForHome() {
+    return this.get<TrackModel[]>('tracks');
+  }
+
+  getAuthor(authorId: string) {
+    return this.get<AuthorModel>(`author/${encodeURIComponent(authorId)}`);
+  }
+
+}
